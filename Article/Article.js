a2edcf7dc49825ef0de0bd3c11dbf5ab7c1d20a6
o1123516766542617 <o1123516766542617@outlook.com>
2019-10-30 17:43:54 -0400

    update utility functions to return new class list

--- Article/Article.js
+++ Article/Article.js
@@ -125,19 +125,31 @@ Element.prototype.hasClass = function (...args) {
 };
 
 Element.prototype.upClass = function (...args) {
+  // modify
   this.classList.add (...args);
+  // return new class list
+  return (this.classList);
 };
 
 Element.prototype.dnClass = function (...args) {
+  // modify
   this.classList.remove (...args);
+  // return new class list
+  return (this.classList);
 };
 
 Element.prototype.toggleClass = function (...args) {
+  // modify
   this.classList.toggle (...args);
+  // return new class list
+  return (this.classList);
 };
 
 Element.prototype.swapClass = function (...args) {
+  // modify
   this.classList.replace (...args);
+  // return new class list
+  return (this.classList);
 };
 
 /***************************************
